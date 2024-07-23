47839a68f5d6f544d3e5ea86ac2d6ad1ef5c05e0
xanzzz12 <122360582+xanzzz12@users.noreply.github.com>
2024-07-23 15:21:33 +0700

    Update hardhat.config.js
    Signed-off-by: xanzzz12 <122360582+xanzzz12@users.noreply.github.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["ce6cbd93848e391fa49e53dd6790f6a24ab3c19e3180caadc2204c7f5d97468b"], //Your private key starting with "0x" 
+      accounts: [""], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
